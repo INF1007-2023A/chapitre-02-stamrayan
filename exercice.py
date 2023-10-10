f5f41f553291fb85ee12f3844e6bee5813cbcbd8
stamrayan <stamrayan@gmail.com>
2023-10-09 23:51:49 -0400

    fini

--- exercice.py
+++ exercice.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
-    return mot
+    nouveau_mot=""
+    for lettre in mot:
+        nouveau_mot+=chr(ord(lettre)-32)
 
+    return nouveau_mot
 
 if __name__ == '__main__':
     mots = [
